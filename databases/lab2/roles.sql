a99b9c4dc7898444df4dd85c20eb8f3a8aad3a88
muratalkhadam <muratalkhadam@gmail.com>
2022-12-14 14:11:21 +0200

    db-lab2: roles added

---
+++ databases/lab2/roles.sql
@@ -0,0 +1,24 @@
+use `db-labs`;
+
+
+create role if not exists doctor, admin;
+
+grant update, select on cards to doctor;
+grant insert, select, delete on conclusions to doctor;
+grant select on * to doctor;
+
+grant insert, select, delete on applications to admin;
+grant insert, select, delete on cards to admin;
+grant insert, select, delete on conclusions to admin;
+grant update, insert, select on discounts to admin;
+grant update, insert, select on doctors to admin;
+grant update, insert, select on patients to admin;
+grant select on admins to admin;
+
+create user if not exists doctor123 identified by 'test1';
+grant doctor to doctor123;
+
+
+
+create user if not exists admin123 identified by 'test2';
+grant admin to admin123;
