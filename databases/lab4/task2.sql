b7bb5508327218bad44b8299bd17e5df70ea2699
muratalkhadam <muratalkhadam@gmail.com>
2022-12-25 21:49:24 +0200

    db lab4: task 2 modified

--- databases/lab4/task2.sql
+++ databases/lab4/task2.sql
@@ -1,6 +1,6 @@
 USE `db-labs`;
 
-
+# a
 CREATE OR REPLACE VIEW patient_info AS
 SELECT p.patient_id,
        CONCAT(p.first_name, ' ', p.last_name) AS full_name,
@@ -13,36 +13,37 @@ ORDER BY p.patient_id;
 
 SELECT *
 FROM patient_info;
-
+# b
 CREATE OR REPLACE VIEW patient_doctor AS
 SELECT p.patient_id,
        CONCAT(p.first_name, ' ', p.last_name) AS patient_name,
+       pi.medical_group,
        CONCAT(d.first_name, ' ', d.last_name) AS doctor_name,
        possible_diagnosis,
        d.specialization
-
 FROM patients p
          JOIN applications a on p.patient_id = a.patient_id
          JOIN doctors d on d.doctor_id = a.doctor_id
+         JOIN patient_info pi on pi.patient_id = a.patient_id
 ORDER BY doctor_name;
 
 SELECT *
 FROM patient_doctor;
-
+# c
 ALTER VIEW patient_info AS
-    SELECT p.patient_id,
-           CONCAT(p.first_name, ' ', p.last_name)        AS full_name,
-           c.height,
-           c.weight,
-           c.medical_group,
-           ROUND(c.weight / POW((c.height / 100), 2), 2) AS bmi
-    FROM patients AS p
-             JOIN cards c on p.patient_id = c.patient_id
-    ORDER BY p.patient_id;
+SELECT p.patient_id,
+       CONCAT(p.first_name, ' ', p.last_name)        AS full_name,
+       c.height,
+       c.weight,
+       c.medical_group,
+       ROUND(c.weight / POW((c.height / 100), 2), 2) AS bmi
+FROM patients AS p
+         JOIN cards c on p.patient_id = c.patient_id
+ORDER BY p.patient_id;
 
 SELECT *
 FROM patient_info;
-
+# d
 /*
 MySQL command to view the definition of a view or table,
 similar to sp_helptext in Microsoft SQL Server
@@ -51,4 +52,5 @@ SHOW CREATE VIEW patient_info;
 
 SELECT *
 FROM information_schema.TABLES
-WHERE TABLE_TYPE = 'VIEW' AND TABLE_SCHEMA = 'db-labs';
+WHERE TABLE_TYPE = 'VIEW'
+  AND TABLE_SCHEMA = 'db-labs';
